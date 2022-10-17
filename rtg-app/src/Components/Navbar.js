fabf84c63c490e9218d0619a0dcb0d1b89377dcf
tymon-silva <tymon.silva@berkeley.edu>
2022-10-16 23:50:27 -0700

    built navbar with proper routing, rendering, and alt pages

--- rtg-app/src/Components/Navbar.js
+++ rtg-app/src/Components/Navbar.js
@@ -1,16 +1,31 @@
 import React from "react";
 import logoV3 from "../Images/logoV3.png";
+import { Link } from 'react-router-dom';
 
 function Navbar() {
 
     return (
-        <nav className="navbar navbar-expand-sm px-5">
-            <div className="conatainer-fluid ">
-                <div className="navbar-brand ">
-                    <img className="logoV3 py-2" src={logoV3} alt="logoV3"></img>
-                </div>
+        <nav className="navbar navbar-expand-lg background">
+            <Link className="navbar-brand" to="/">
+                <img className="logoV3 p-3" src={logoV3} alt="Home" to="/"></img>
+            </Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link className="nav-link" to="/about">About</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/placeholder">Placeholder</Link>
+                    </li>
+                </ul>
             </div>
-        </nav>
+        </nav> 
     )
 
 };
