26afb1392a7a81706bee0abed92c72d2a6c250db
tymon-silva <tymon.silva@berkeley.edu>
2022-10-16 23:50:01 -0700

    add routes and to App

--- rtg-app/src/App.js
+++ rtg-app/src/App.js
@@ -1,17 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.svg';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Placeholder from './Pages/Placeholder';
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          TitleAI coming soon!.
-        </p>
-      </header>
-    </div>
+    <>
+      <Navbar></Navbar>
+      <div className='col-lg p-4'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/placeholder' element={<Placeholder />} />
+        </Routes>
+      </div>
+      {/* <About></About> */}
+    </>
+    
   );
 }
+
 
 export default App;
