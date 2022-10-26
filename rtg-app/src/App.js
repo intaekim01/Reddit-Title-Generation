cf7b41f9e7c6261f8d1b086ae2de8c0d9f66f1fb
tymon-silva <tymon.silva@berkeley.edu>
2022-10-26 10:56:02 -0700

    cleanup app.js

--- rtg-app/src/App.js
+++ rtg-app/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
 import Navbar from './Components/Navbar';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Placeholder from './Pages/Placeholder';
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("")
+
   return (
+   
     <>
       <Navbar></Navbar>
       <Routes>
@@ -16,7 +19,6 @@ function App() {
         <Route path='/about' element={<About />} />
         <Route path='/placeholder' element={<Placeholder />} />
       </Routes>
-      {/* <About></About> */}
     </>
     
   );
