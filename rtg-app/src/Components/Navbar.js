4c171d635576dfd0d6ff30dec55bed73ddcb267f
tymon-silva <tymon.silva@berkeley.edu>
2022-10-30 21:43:46 -0700

    formatting

--- rtg-app/src/Components/Navbar.js
+++ rtg-app/src/Components/Navbar.js
@@ -9,15 +9,15 @@ function Navbar() {
             <Link className="navbar-brand" to="/">
                 <img className="logoV3 p-3" src={logoV3} alt="Home" to="/"></img>
             </Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler mx-4" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <div className="collapse navbar-collapse text-end" id="navbarNav">
+                <ul className="navbar-nav mb-2 mb-lg-0 mx-4 mx-lg-0">
                     <li className="nav-item">
                         <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <Link className="nav-link" to="/about">About</Link>
                     </li>
                     <li className="nav-item">
