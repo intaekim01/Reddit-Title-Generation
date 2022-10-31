77b3216f1ec089c4e119d32b9951ef8ee09e0a0f
tymon-silva <tymon.silva@berkeley.edu>
2022-10-30 21:41:30 -0700

    moved bootstrap imports to index and removed strict mode

--- rtg-app/src/index.js
+++ rtg-app/src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
