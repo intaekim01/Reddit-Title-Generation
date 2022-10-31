c39d0313db5fe09b841f0aa099bb74e9383f38da
tymon-silva <tymon.silva@berkeley.edu>
2022-10-30 21:43:24 -0700

    set post state at parent component and passed to child

--- rtg-app/src/Components/RichText.js
+++ rtg-app/src/Components/RichText.js
@@ -1,31 +1,25 @@
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import {CKEditor} from "@ckeditor/ckeditor5-react";
 import parse from "html-react-parser";
-import { useState } from "react";
 
-function RichText() {
-    const [text, setText] = useState("")
+function RichText({post, setPost}) {
+    console.log('Richtext component rendering...')
+    
     return (
-        <div className="App">
         <div className='editor'>
             <CKEditor
                 editor={ClassicEditor}
-                data={text}
+                data={post}
                 onChange={(event, editor) => {
-                    const data = editor.getData()
-                    setText(data)
+                    const data = editor.getData();
+                    setPost(data);
                     }
                 }
             />
         </div>
-        <div>
-            <h2>Content</h2>
-            <p>{parse(text)}</p>
-        </div>
-      </div>
-
 
     )
+    
   }
   
   export default RichText;
