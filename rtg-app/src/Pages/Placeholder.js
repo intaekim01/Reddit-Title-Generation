7d22326a29ee6e6dd8f2391dd48caa09267beac9
tymon-silva <tymon.silva@berkeley.edu>
2022-11-27 19:37:35 -0800

    cleared testing on Placholder page

--- rtg-app/src/Pages/Placeholder.js
+++ rtg-app/src/Pages/Placeholder.js
@@ -1,38 +1,16 @@
 import { Toast } from 'bootstrap';
 import IconV3 from "../Images/IconV3.png"; 
+import { ThreeCircles } from 'react-loader-spinner'
 
 function Placeholder() {
 
-    const toastTrigger = document.getElementById('liveToastBtn')
-    const toastLiveExample = document.getElementById('liveToast')
-    if (toastTrigger) {
-        toastTrigger.addEventListener('click', () => {
-            const toast = new Toast(toastLiveExample)
-    
-        toast.show()
-      })
-    }
-
     return (
         <div className="container-fluid page">
+
             <div className="container text-center">
                 <h1>Placeholder content here</h1>
             </div>
-            <button type="button" class="btn btn-primary" id="liveToastBtn">Show live toast</button>
-
-            <div class="toast-container position-fixed bottom-0 end-0 p-3">
-                <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-                    <div class="toast-header">
-                        <img className="IconV3.png rounded me-2" src={IconV3} width="17" height="17"></img>
-                        <strong class="me-auto"></strong>
-                        <strong>title copied!</strong>
-                        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                    </div>
-                    {/* <div class="toast-body">
-                        Title copied!
-                    </div> */}
-                </div>
-            </div>
+            
             
         </div>
     )
