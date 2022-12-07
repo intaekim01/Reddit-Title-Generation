7c438b3f13e7948f77523e5874ffdccf573751a1
tymon-silva <tymon.silva@berkeley.edu>
2022-12-06 17:17:46 -0800

    revamped team page

--- rtg-app/src/Pages/Team.js
+++ rtg-app/src/Pages/Team.js
@@ -2,13 +2,41 @@
 function Team() {
 
     return (
-        <div className="container-fluid page">
+        <div className="container-fluid page pb-5">
+            <div class="container" >
+                
+                <div class="row">
+                    <h2 class="py-5">About the team</h2>
+                </div>
 
-            <div className="container text-center">
-                <h1>Team content here</h1>
+                <div class="row">
+                    <h5>Members</h5>
+                </div>
+                
+                <div>
+                    <p><a href="https://www.ischool.berkeley.edu/people/joy-jiang#profile-main">Intae Kim</a></p>
+                    <ul>
+                        <li>Contact: intaekim@ischool.berkeley.edu</li>
+                        <li>Employment: </li>
+                    </ul>
+                    <p><a href="https://www.ischool.berkeley.edu/people/justine-schabel">George Rodriguez</a></p>
+                    <ul>
+                        <li>Contact: georgerodriguez@ischool.berkeley.edu</li>
+                        <li>Employement: </li>
+                    </ul>
+                    <p><a href="https://www.ischool.berkeley.edu/people/tymon-silva#profile-main">Tymon Silva</a></p>
+                    <ul>
+                        <li>Contact: tymon.silva@berkeley.edu</li>
+                        <li>Employment: Industrial Engineer/Developer, Boeing</li>
+                    </ul>
+                    <p><a href="https://www.ischool.berkeley.edu/people/tymon-silva#profile-main">Qi Yao</a></p>
+                    <ul>
+                        <li>Contact: aliviny@berkeley.edu</li>
+                        <li>Employment: </li>
+                    </ul>
+                </div>
+
             </div>
-            
-            
         </div>
     )
 
