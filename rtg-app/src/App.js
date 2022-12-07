f25a7ce015e2c3b776133ee325a879ac5181aa93
tymon-silva <tymon.silva@berkeley.edu>
2022-12-06 17:19:41 -0800

    hiding comingSoon page import

--- rtg-app/src/App.js
+++ rtg-app/src/App.js
@@ -5,7 +5,7 @@ import Home from './Pages/Home';
 import TitleGenerator from './Pages/TitleGenerator';
 import About from './Pages/About';
 import Team from './Pages/Team';
-import ComingSoon from './Pages/ComingSoon';
+// import ComingSoon from './Pages/ComingSoon';
 import { Route, Routes } from "react-router-dom";
 import React, { useState } from "react";
 
